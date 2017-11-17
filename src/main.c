c873582a26d64810967afb2be2450e71fc98c6be
David Banks <dave@hoglet.com>
2017-11-17 13:17:02 +0000

    Reworked the sampling to improve efficient (no functional change)

--- src/main.c
+++ src/main.c
@@ -22,16 +22,16 @@ const int do_emulate = 1;
 // TODO: all the pc prediction stuff could be pushed down into the emulation
 
 static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, int write_accumulator, int write_count, int operand, int num_cycles) {
-   
+
    // Predicted PC value
    static int pc = -1;
-   
+
    int offset;
    char target[16];
 
    // lookup the entry for the instruction
-   InstrType *instr = &instr_table[opcode];   
-   
+   InstrType *instr = &instr_table[opcode];
+
    // For instructions that push the current address to the stack we
    // can use the stacked address to determine the current PC
    int newpc = -1;
@@ -42,7 +42,7 @@ static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, in
       // JSR
       newpc = (write_accumulator - 2) & 0xffff;
    }
-   
+
    // Sanity check the current pc prediction has not gone awry
    if (newpc >= 0) {
       if (pc >= 0 && pc != newpc) {
@@ -50,13 +50,13 @@ static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, in
          pc = newpc;
       }
    }
-   
+
    if (pc < 0) {
       printf(" ????: ");
    } else {
       printf(" %04X: ", pc);
    }
-   
+
    int numchars = 0;
    if (write_count == 3 && opcode != 0) {
       // Annotate an interrupt
@@ -64,7 +64,7 @@ static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, in
       if (do_emulate) {
          em_interrupt(write_accumulator & 0xff);
       }
-   } else {      
+   } else {
       // Annotate a normal instruction
       const char *mnemonic = instr->mnemonic;
       const char *fmt = instr->fmt;
@@ -109,7 +109,7 @@ static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, in
          numchars = printf(fmt, mnemonic, op1, op2);
          break;
       }
-      
+
       // Emulate the instruction
       if (do_emulate) {
          if (instr->emulate) {
@@ -128,7 +128,7 @@ static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, in
          }
       }
    }
-   
+
    if (do_emulate) {
       // Pad opcode to 14 characters, to match python
       while (numchars++ < 14) {
@@ -138,7 +138,7 @@ static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, in
    } else {
       printf("\n");
    }
-   
+
    // Look for control flow changes and update the PC
    if (opcode == 0x40 || opcode == 0x00 || opcode == 0x6c || opcode == 0x7c || write_count == 3) {
       // RTI, BRK, INTR, JMP (ind), JMP (ind, X), IRQ/NMI/RST
@@ -168,25 +168,24 @@ static void analyze_cycle(int opcode, int op1, int op2, int read_accumulator, in
 }
 
 void decode(FILE *stream) {
-   
+
    // Count of the 6502 bus cycles
    int cyclenum             = 0;
-   
+
    // Cycle count of the last sync, so we know the instruction cycle count
    int last_sync_cyclenum   = 0;
-   
+
    // State to decode the 6502 bus activity
    int cycle                = Cycle_MEMRD;
-   int opcount              = 0;
    int opcode               = -1;
+   int opcount              = 0;
    int op1                  = 0;
    int op2                  = 0;
    int operand              = 0;
    int write_count          = 0;
    int read_accumulator     = 0;
    int write_accumulator    = 0;
-   
-   
+
    // Pin mappings into the 16 bit words
    // TODO: make these configurable
    int idx_data  =  0;
@@ -194,76 +193,77 @@ void decode(FILE *stream) {
    int idx_sync  =  9;
    int idx_rdy   = 10;
    int idx_phi2  = 11;
-   
+
    // Pin values
    int bus_data  =  0;
    int pin_rnw   =  0;
    int pin_sync  =  0;
    int pin_rdy   =  0;
    int pin_phi2  =  0;
-   
-   // To look for transitions in Phi2
-   int last_phi2 = -1;
-   
+
    int num;
-   
+
+   // The previous sample of the 16-bit capture (async sampling only)
+   uint16_t last_sample = -1;
+
+   // The previous sample of phi2 (async sampling only)
+   int last_phi2 = -1;
+
    while ((num = fread(buffer, sizeof(uint16_t), BUFSIZE, stream)) > 0) {
-      
+
       uint16_t *sampleptr = &buffer[0];
-      
+
       while (num-- > 0) {
-         
+
+         // The current 16-bit capture sample
          uint16_t sample = *sampleptr++;
-         
+
          // Phi2 is optional
          // - if asynchronous capture is used, it must be connected
          // - if synchronous capture is used, it must not connected
          if (idx_phi2 < 0) {
-            
+
             // If Phi2 is not present, use the pins directly
             bus_data  = (sample >> idx_data) & 255;
-            pin_rnw   = (sample >> idx_rnw) & 1;
+            pin_rnw   = (sample >> idx_rnw ) & 1;
             pin_sync  = (sample >> idx_sync) & 1;
-            pin_rdy   = (sample >> idx_rdy) & 1;
-            
+            pin_rdy   = (sample >> idx_rdy ) & 1;
+
          } else {
-            
-            // TODO: make this faster by using last_sample
-            
+
             // If Phi2 is present, look for the falling edge, and proceed with the previous sample
             pin_phi2 = (sample >> idx_phi2) & 1;
             if (pin_phi2 == 1 || last_phi2 != 1) {
                last_phi2 = pin_phi2;
-               bus_data  = (sample >> idx_data) & 255;
-               pin_rnw   = (sample >> idx_rnw) & 1;
-               pin_sync  = (sample >> idx_sync) & 1;
-               pin_rdy   = (sample >> idx_rdy) & 1;
+               last_sample = sample;
                continue;
             }
+
+            // At this point, last_phi2 = 1 and phi2 = 0 (i.e. falling edge)
+            last_phi2 = 0;
+
+            // Sample the control signals before the clock edge
+            pin_rnw   = (last_sample >> idx_rnw ) & 1;
+            pin_sync  = (last_sample >> idx_sync) & 1;
+            pin_rdy   = (last_sample >> idx_rdy ) & 1;
+            // Sample write data before the clock edge, and read data after the clock edge
+            bus_data  = ((pin_rnw ? sample : last_sample) >> idx_data ) & 255;
          }
-         
-         // Resample read data just after the falling edge, as there should be reasonable hold time
-         if (pin_rnw == 1) {
-            bus_data  = (sample >> idx_data) & 255;
-         }
-         
-         // At this point, either phi2 is not connected, or last_phi2 = 1 and phi2 = 0 (i.e. falling edge)
-         last_phi2 = 0;
-         
+
          // Ignore the cycle if RDY is low
          if (pin_rdy == 0)
             continue;
-         
+
          // Sync indicates the start of a new instruction, the following variables pertain to the previous instruction
          // opcode, op1, op2, read_accumulator, write_accumulator, write_count
-         
+
          if (pin_sync == 1) {
-            
+
             if (opcode >= 0) {
                analyze_cycle(opcode, op1, op2, read_accumulator, write_accumulator, write_count, operand, cyclenum - last_sync_cyclenum);
             }
             last_sync_cyclenum  = cyclenum;
-            
+
             // Start decoding a new instruction
             cycle             = Cycle_FETCH;
             opcode            = bus_data;
@@ -272,18 +272,18 @@ void decode(FILE *stream) {
             operand           = -1;
             read_accumulator  = 0;
             write_accumulator = 0;
-            
+
          } else if (pin_rnw == 0) {
             cycle = Cycle_MEMWR;
             write_count += 1;
             write_accumulator = (write_accumulator << 8) | bus_data;
-            
+
          } else if (cycle == Cycle_FETCH && opcount > 0) {
             cycle = Cycle_OP1;
             opcount -= 1;
             op1 = bus_data;
             operand = bus_data;
-            
+
          } else if (cycle == Cycle_OP1 && opcount > 0) {
             if (opcode == 0x20) { // JSR is <opcode> <op1> <dummp stack rd> <stack wr> <stack wr> <op2>
                cycle = Cycle_MEMRD;
@@ -292,7 +292,7 @@ void decode(FILE *stream) {
                opcount -= 1;
                op2 = bus_data;
             }
-            
+
          } else {
             if (opcode == 0x20){ // JSR, see above
                cycle = Cycle_OP2;
@@ -304,7 +304,7 @@ void decode(FILE *stream) {
                read_accumulator = (read_accumulator >> 8) | (bus_data << 16);
             }
          }
-         
+
          // Increment the cycle number (used only to detect taken branches)
          cyclenum += 1;
       }
