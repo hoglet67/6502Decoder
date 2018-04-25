ccd4a41b8860c99672388af537760f6a6bcab4d3
David Banks <dave@hoglet.com>
2018-04-25 10:08:30 +0100

    Fixed build.sh script to run on Windows

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-gcc -Wall -O3 -D_GNU_SOURCE -o decode6502 src/main.c src/em_6502.c src/profiler.c src/profiler_instr.c src/profiler_block.c src/profiler_call.c -lm
+LIBS="-lm"
+
+if [[ $OS = *"Windows"* ]]; then
+  LIBS="$LIBS -largp"
+fi
+
+gcc -Wall -O3 -D_GNU_SOURCE -o decode6502 src/main.c src/em_6502.c src/profiler.c src/profiler_instr.c src/profiler_block.c src/profiler_call.c $LIBS
