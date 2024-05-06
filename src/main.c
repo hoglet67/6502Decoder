hoglet67/6502Decoder

commit c1d91c5bffba2d3ce88653b45e51e43fd2787b9b

The -y option now includes cycles where rdy=0

src/main.c
@@ -707,6 +707,7 @@ static int analyze_instruction(sample_t *sample_q, int num_samples, int rst_seen
    int intr_seen = em->match_interrupt(sample_q, num_samples);
 
    int num_cycles;
+   int real_cycles;
 
    // This is a rather ugly hack to cope with a decode failure on Arlet's core.
    //
@@ -779,6 +780,8 @@ static int analyze_instruction(sample_t *sample_q, int num_samples, int rst_seen
       // Handle a normal instruction
       em->emulate(sample_q, num_cycles, &instruction);
    }
+
+   real_cycles = sample_q[num_cycles].sample_count - sample_q[0].sample_count;
 
    // Sanity check the pc prediction has not gone awry
    // (e.g. in JSR the emulation can use the stacked PC)
@@ -916,7 +919,7 @@ static int analyze_instruction(sample_t *sample_q, int num_samples, int rst_seen
          *bp++ = ':';
          *bp++ = ' ';
          // No instruction is more then 8 cycles
-         write_hex1(bp++, num_cycles);
+         write_hex1(bp++, real_cycles);
       }
       // Show register state
       if (fail || arguments.show_state) {

commit cf641932b273c07f6df1c42ef7cb6e20c37a5ce7

Fix incorrect cycle counts in async mode (introduced in c1d91c5b)

src/main.c
@@ -1304,9 +1304,9 @@ void decode(FILE *stream) {
                      s.data = (skew_buffer[s.rnw == 0 ? wrdata_head : rddata_head] >> idx_data) & 255;
                      queue_sample(&s);
                   }
+                  s.sample_count++;
                }
             }
-            s.sample_count++;
             // Increment the circular buffer pointers in lock-step to keey the skew constant
             tail        = (tail        + 1) & (SKEW_BUFFER_SIZE - 1);
             head        = (head        + 1) & (SKEW_BUFFER_SIZE - 1);