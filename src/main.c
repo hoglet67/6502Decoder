cce039d18a7c1c73ab89c4b08f6d80f2a7470ee0
David Banks <dave@hoglet.com>
2025-11-04 19:24:09 +0000

    SC/MP: Updated handling of undocumented instructions

--- src/main.c
+++ src/main.c
@@ -1820,8 +1820,8 @@ int main(int argc, char *argv[]) {
    }
 
    // Validate options compatibility with CPU
-   if (arguments.cpu_type != CPU_6502 && arguments.cpu_type != CPU_6800 && arguments.undocumented) {
-      fprintf(stderr, "--undocumented is only applicable to the 6502/6800\n");
+   if (arguments.cpu_type != CPU_6502 && arguments.cpu_type != CPU_6800 && arguments.cpu_type != CPU_SCMP && arguments.undocumented) {
+      fprintf(stderr, "--undocumented is only applicable to the 6502/6800/SCMP\n");
       return 1;
    }
    if (arguments.cpu_type == CPU_65C816) {
